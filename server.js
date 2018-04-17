de4fb8f4bbfd36fd666555fae7007ea4dbd634be
Jalil <ja@Jas-MacBook-Pro.local>
2018-04-17 03:13:35 -0400

    added to git pages

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+
+var PORT = process.env.PORT || 3000;
+
+ 
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+ 
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+require("./app/routing/html-routes.js")(app);
+
+app.listen(PORT, function(){
+    console.log("App listing on PORT:" + PORT);
+}); 
